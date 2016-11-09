05ae3cac35baccd6a1cf63b8271a97d3065ccc5c
江智彬 <jiangzhibin2014.xujie@gmail.com>
2016-11-10 00:23:22 +0800

    test concurrency

--- test_concurrency.py
+++ test_concurrency.py
@@ -1,7 +1,7 @@
 #coding=utf-8
 import sys, time
 from datetime import datetime as dt
-import MySQLdb as mdb
+# import MySQLdb as mdb
 from queue import Queue
 from multiprocessing.dummy import Pool as MPool
 from wetchat_spider import get_article_info
@@ -20,7 +20,7 @@ URL_QUEUE = Queue()
 ARTICLE_QUEUE = Queue()
 TOPIC_QUEUE = Queue()
 
-def wxurl_generator(keyword):
+def test_wxurl_generator(keyword):
 	"""
 	Producer for urls and topics
 	"""
@@ -30,7 +30,7 @@ def wxurl_generator(keyword):
 	for url in sougou_result['data']['urls']:
 		URL_QUEUE.put("%s|%s" % (sougou_url, url))  # merge two url
 
-def wxarticle_generator(abuyunproxy={}):
+def test_wxarticle_generator(abuyunproxy={}):
 	"""
 	Consummer for urls and Producer for articles
 	"""
@@ -40,41 +40,41 @@ def wxarticle_generator(abuyunproxy={}):
 		wetchat_result = get_article_info(s_url, w_url, proxy=abuyun_proxy)
 		ARTICLE_QUEUE.put(wetchat_result['data'])  # generate article data and put into queue
 
-def article_db_writer():
+def test_article_db_writer():
 	"""
 	Consummer for articles
 	"""
-	with mdb.connect(host = "192.168.1.103", user="web", 
-        passwd="Crawler@test1", db="webcrawler", charset="utf8" ) as conn:
-		while not ARTICLE_QUEUE.empty():
-			article_record = ARTICLE_QUEUE.get_nowait()
-			write_article_into_db(conn, article_record)
+	while not ARTICLE_QUEUE.empty():
+		article_record = ARTICLE_QUEUE.get_nowait()
+		print "The article has %d like, %d read and %d characters\n" % (
+                    article_record.get('like_num', -1), 
+                    article_record.get('read_num', -1), 
+                    len(article_record.get('content', '')))
 
-def topic_db_writer():
+def test_topic_db_writer():
 	"""
 	Consummer for topics
 	"""
-	with mdb.connect(host = "192.168.1.103", user="web", 
-        passwd="Crawler@test1", db="webcrawler", charset="utf8" ) as conn:
-		while not TOPIC_QUEUE.empty():
-			topic_record = TOPIC_QUEUE.get_nowait()
-			write_topic_into_db(conn, topic_record)
+	while not TOPIC_QUEUE.empty():
+		topic_record = TOPIC_QUEUE.get_nowait()
+		print "Topic %s has %d wx_urls" % (
+			topic_record['search_keyword'], len(topic_record['urls']))
 
-def run_all_worker(conn, concurrency=4):
+def run_all_worker(concurrency=4):
 	abuyun_proxy = gen_abuyun_proxy()
     try:
     	# Producer is on !!!
-        list_of_kw = read_topics_from_db(conn)
+        list_of_kw = ["特朗普", "45任总统", "暴走大事件", "王尼玛", "阴阳师"]
 		pool = MPool(concurrency)  # Processes pool
-        pool.map(wxurl_generator, list_of_kw)  # Keep up generate keywords
+        pool.map(test_wxurl_generator, list_of_kw)  # Keep up generate keywords
         pool.close()
         pool.join()  # why join
     except KeyboardInterrupt:
         print "Interrupted by you and quit in force, but save the results"
     # Consummer followes
-    wxurl_generator(abuyun_proxy)
-    article_db_writer(conn)
-    topic_db_writer(conn)
+    test_wxurl_generator(abuyun_proxy)
+    test_article_db_writer()
+    test_topic_db_writer()
 
 
 if __name__=="__main__":
